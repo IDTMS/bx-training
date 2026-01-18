48cce6ad723774257e157fb1059f72d2e5c32302
marcusllittle <marcusllittle@gmail.com>
2026-01-18 11:59:03 -0500

    add hello script

---
+++ foundations/scripts/hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def main() -> None:
+    banner = r"""
+ __        __   _                              
+ \ \      / /__| | ___ ___  _ __ ___   ___   
+  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ 
+   \ V  V /  __/ | (_| (_) | | | | | |  __/ 
+    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|   
+"""
+    print(banner)
+    print("Welcome BX Session 0: environment online. Let's build something dope")
+
+
+if __name__ == "__main__":
+    main()
